d7fce5987b2a4a702241a994f1e81d27565192db
Azamat Zarlykov <azarlykovcz@gmail.com>
2022-04-05 00:01:36 +0200

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.tetris {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens project.tetris to javafx.fxml;
+    exports project.tetris;
+}
